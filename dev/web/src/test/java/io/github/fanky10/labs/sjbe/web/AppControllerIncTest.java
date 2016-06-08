55b69149a604ef23e90f32f6458c9c7c2332f6fe
Facundo Scoccia <fanky10@gmail.com>
2016-06-07 22:06:56 -0300

    Fix project build

--- dev/web/src/test/java/io/github/fanky10/labs/sjbe/web/AppControllerIncTest.java
+++ dev/web/src/test/java/io/github/fanky10/labs/sjbe/web/AppControllerIncTest.java
@@ -5,6 +5,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import javax.inject.Inject;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,10 +15,12 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import io.github.fanky10.labs.sjbe.Application;
+import io.github.fanky10.labs.sjbe.config.AppConfig;
 import io.github.fanky10.labs.sjbe.web.rest.AppController;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -24,22 +28,30 @@ import io.github.fanky10.labs.sjbe.web.rest.AppController;
 @WebAppConfiguration
 @IntegrationTest
 public class AppControllerIncTest {
-	
+
+	@Inject
+	private AppConfig appConfig;
+
 	private MockMvc restUserMockMvc;
 
-    @Before
-    public void setup() {
-    	AppController appController = new AppController();
-        this.restUserMockMvc = MockMvcBuilders.standaloneSetup(appController).build();
-    }
+	@Before
+	public void setup() {
+		AppController appController = new AppController();
+		ReflectionTestUtils.setField(appController, "appConfiguration",
+				appConfig);
+		this.restUserMockMvc = MockMvcBuilders.standaloneSetup(appController)
+				.build();
+	}
 
-    @Test
-    public void testGetApplication() throws Exception {
-        restUserMockMvc.perform(get("/api/application")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$.version").value("1.0.1"));
-    }
+	@Test
+	public void testGetApplication() throws Exception {
+		restUserMockMvc
+				.perform(
+						get("/api/application").accept(
+								MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json"))
+				.andExpect(jsonPath("$.version").value("1.0.1"));
+	}
 
 }
